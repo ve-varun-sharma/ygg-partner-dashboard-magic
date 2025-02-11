5cb4a2e504014b99b5baad8968e3256ce0da6939
Varun Sharma <62218708+ve-varun-sharma@users.noreply.github.com>
2025-02-10 17:32:16 -0800

    feat: remove logos for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
     <main>
       <Header />
       <Hero />
-      <Logos />
+      {/* <Logos /> */}
       <UseCases />
       {/* <Features /> */}
       <Statistics />
